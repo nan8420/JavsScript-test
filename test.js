22ffcaf842879a4510574c85c42f39d39e500da6
Kim Gun Hee <nan8420@gmail.com>
2023-07-25 23:06:47 +0900

    test

--- test.js
+++ test.js
@@ -1,5 +1,16 @@
 function solution() {
-  console.log("num1:");
+  let insects = [
+    { label: "쌍별귀뚜라미", value: "INSECT5" },
+    { label: "누에", value: "INSECT3" },
+    { label: "누에", value: "INSECT3" },
+    { label: "누에", value: "INSECT3" },
+    { label: "누에", value: "INSECT3" },
+  ];
+
+  const filterInsect = insects.filter(
+    (v, i) => insects.findIndex((x) => v.label === x.label) === i
+  );
+  console.log(" filterInsect:", filterInsect);
 }
 
 solution();
